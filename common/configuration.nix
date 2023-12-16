c5d6b1f575a1314e191b2e352befe6f4914292f7
Felix Huettner <huettner94@gmx.de>
2023-12-16 13:52:42 +0100

    wrong import

--- common/configuration.nix
+++ common/configuration.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./home.nix ./development.nix ];
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
