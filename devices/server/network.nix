b6d33da21886ca1383593d8644c3679f6a9d36b8
Felix Huettner <huettner94@gmx.de>
2025-06-13 22:24:44 +0200

    internet config for dgn

--- devices/server/network.nix
+++ devices/server/network.nix
@@ -3,27 +3,6 @@
 {
   environment.systemPackages = with pkgs; [ ppp ];
 
-  services.dnsmasq = {
-    enable = true;
-    alwaysKeepRunning = true;
-    settings = {
-      interface = "br-int";
-      cache-size = 10000;
-      domain = "int.eurador.de";
-
-      # disable dns, as that is done by adguard.
-      port = 0;
-
-      # the default gateway and dns server
-      dhcp-option = [ "3,192.168.0.1" "6,192.168.0.1" ];
-
-      # default dhcp range
-      dhcp-range = "192.168.0.40,192.168.0.200,24h";
-      # APSystems ECU-B (balkonkraftwerk)
-      dhcp-host = "b4:8a:0a:ee:91:16,192.168.0.31";
-    };
-  };
-
   services.adguardhome = {
     enable = true;
     host = "192.168.0.1";
@@ -59,21 +38,33 @@
 
             # deny access from outside (connections are already allowed)
             iifname "enp0s31f6" counter drop
+            iffname "ppp0" counter drop
+            iifname "dgn-aftr" counter drop
           }
 
           chain forward {
             type filter hook forward priority filter; policy accept;
             ct state established,related accept
 
+            # MSS Clamping towards DGN
+            tcp flags syn oifname {
+                    "ppp0",
+                    "dgn-aftr"
+            } tcp option maxseg size set rt mtu
+
             # Allow trusted network WAN access
             iifname {
                     "br-int",
             } oifname {
                     "enp0s31f6",
+                    "ppp0",
+                    "dgn-aftr",
             } counter accept
 
             # deny access from outside (connections are already allowed)
             iifname "enp0s31f6" counter drop
+            iifname "ppp0" counter drop
+            iifname "dgn-aftr" counter drop
           }
         }
 
@@ -101,33 +92,56 @@
           Name = "br-int";
         };
       };
+      "30-dgn-vlan7" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "dgn-vl7";
+        };
+        vlanConfig = {
+          Id = 7;
+        };
+      };
+      "70-dgn-aftr" = {
+        netdevConfig = {
+          Kind = "ip6tnl";
+          Name = "dgn-aftr";
+        };
+        tunnelConfig = {
+          Mode = "ipip6";
+          Remote = "2a01:41e3:ffff:cafe:face::3";
+        };
+      };
     };
 
     networks = {
       # bridge ports
-      "30-enp1s0f0" = {
+      "40-enp1s0f0" = {
         matchConfig.Name = "enp1s0f0";
         networkConfig.Bridge = "br-int";
         linkConfig.RequiredForOnline = "enslaved";
       };
-      "30-enp1s0f1" = {
+      "40-enp1s0f1" = {
         matchConfig.Name = "enp1s0f1";
         networkConfig.Bridge = "br-int";
         linkConfig.RequiredForOnline = "enslaved";
       };
-      "30-enp1s0f2" = {
+      "40-enp1s0f2" = {
         matchConfig.Name = "enp1s0f2";
         networkConfig.Bridge = "br-int";
         linkConfig.RequiredForOnline = "enslaved";
       };
-      "30-enp1s0f3" = {
+
+      # DGN Upstream
+      "40-enp1s0f3" = {
         matchConfig.Name = "enp1s0f3";
-        networkConfig.Bridge = "br-int";
-        linkConfig.RequiredForOnline = "enslaved";
+        networkConfig = {
+          VLAN = "dgn-vl7";
+          DHCP = "no";
+        };
       };
 
       # fritzbox upstream
-      "30-enp0s31f6" = {
+      "40-enp0s31f6" = {
         matchConfig.Name = "enp0s31f6";
         address = [ "192.168.10.10/24" ];
         gateway = [ "192.168.10.1" ];
@@ -135,11 +149,103 @@
       };
 
       # internal
-      "40-br-int" = {
+      "50-br-int" = {
         matchConfig.Name = "br-int";
         bridgeConfig = { };
         address = [ "192.168.0.1/24" ];
         linkConfig.RequiredForOnline = "routable";
+        networkConfig = {
+          IPv6AcceptRA = "yes";
+          IPv6SendRA = "yes";
+          DHCPPrefixDelegation = "yes";
+          DHCPServer = "true";
+        };
+        dhcpServerConfig = {
+          PoolOffset = "40";
+          PoolSize = "160";
+          EmitDNS = "true";
+          DNS = "192.168.0.1";
+          EmitRouter = "true";
+          Router = "192.168.0.1";
+        };
+        dhcpServerStaticLeases = [
+          {
+            Address = "192.168.0.31";
+            MACAddress = "b4:8a:0a:ee:91:16";
+          }
+        ];
+        ipv6SendRAConfig = {
+          EmitDNS = "true";
+          DNS = "fe80::1";
+        };
+      };
+
+      # DGN PPP
+      "60-ppp-dgn" = {
+        matchConfig = {
+          Name = "ppp0";
+          Type = "ppp";
+        };
+        networkConfig = {
+          DHCP = "ipv6";
+          IPv6AcceptRA = "yes";
+          IPv6PrivacyExtensions = "yes";
+          KeepConfiguration = "yes";
+        };
+        dhcpV6Config = {
+          UseDelegatedPrefix = "true";
+          WithoutRA = "solicit";
+        };
+      };
+
+      # DGN AFTR
+      "80-dgn-aftr" = {
+        matchConfig = {
+          Name = "dgn-aftr";
+        };
+        addresses = [
+          {
+            Address = "192.0.0.2/29";
+            Peer = "192.0.0.1";
+          }
+        ];
+        #gateway = [ "192.0.0.1" ];
+        #linkConfig.RequiredForOnline = "routable";
+      };
+    };
+  };
+
+  # PPPoE for DGN
+  services.pppd = {
+    enabled = true;
+    peers = {
+      dgn = {
+        autostart = true;
+        enable = true;
+        config = ''
+          plugin rp-pppoe.so dgn-vl7
+          
+          # pppd supports multiple ways of entering credentials,
+          # this is just 1 way
+          name "P2004329@dgn.digital"
+          password "53261N5GNVDBa3N9"
+
+          noipdefault
+          # defaultroute
+          # replacedefaultroute
+          hide-password
+          lcp-echo-interval 20
+          lcp-echo-failure 3
+          connect /bin/true
+          noauth
+          persist
+          noaccomp
+          default-asyncmap
+
+          nodetach
+          persist
+
+        '';
       };
     };
   };
